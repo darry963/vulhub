fdede9b93d32f06b3abfe7c0e35457a658198fda
phith0n <phith0n.ph2f@gmail.com>
2017-05-17 04:35:12 +0800

    curl follow redirect

--- php/ssrf/www/index.php
+++ php/ssrf/www/index.php
@@ -3,6 +3,7 @@ $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $_GET["url"]);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($ch, CURLOPT_HEADER, 0);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE);
 $output = curl_exec($ch);
 curl_close($ch);
 
